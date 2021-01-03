3a9f14e85f361f76516da4550c19a6d3b70c10be
zhaoamy <zhaoamy@localhost>
2021-01-02 17:56:09 -0800

    -_-

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -32,7 +32,7 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		if(s1.length() > s2.length()) {
+		if (s1.length() > s2.length()) {
 			return s1;
 		} else if (s2.length() > s1.length()) {
 			return s2;
@@ -40,27 +40,56 @@ public class StringMethods {
 		return "equal";
 	}
 
-	
-	// if String s contains the word "underscores", change all of the spaces to underscores
+	// if String s contains the word "underscores", change all of the spaces to
+	// underscores
 	public static String formatSpaces(String s) {
-		return null;
+		String s1 = "";
+		if (s.contains("underscores")) {
+			s1 = s.replace(' ', '_');
+			return s1;
+		}
+		return s;
 	}
 
-	
-	// Return the name of the person whose LAST name would appear first if they were in alphabetical order
+	// Return the name of the person whose LAST name would appear first if they were
+	// in alphabetical order
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
-		return null;
+		s1 = s1.trim();
+		s2 = s2.trim();
+		s3 = s3.trim();
+
+		String[] array1 = s1.split(" ");
+		String s01 = array1[1];
+		String[] array2 = s2.split(" ");
+		String s02 = array2[1];
+		String[] array3 = s3.split(" ");
+		String s03 = array3[1];
+
+		if (s01.charAt(0) < s02.charAt(0) && s01.charAt(0) < s03.charAt(0)) {
+			return s1;
+		} else if (s02.charAt(0) < s01.charAt(0) && s02.charAt(0) < s03.charAt(0)) {
+			return s2;
+		} else if (s03.charAt(0) < s01.charAt(0) && s03.charAt(0) < s02.charAt(0)) {
+			return s3;
+		} else {
+			return null;
+		}
 	}
-	
-	
+
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		return 0;
+		int sum = 0;
+		for(int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if(c >= 48 && c <= 57) {
+				sum+= Character.getNumericValue(c);
+			}
+		}
+		return sum;
 	}
-	
-	
+
 	// Return the number of times String substring appears in String s
 	public static int substringCount(String s, String substring) {
 		return 0;
@@ -76,13 +105,11 @@ public class StringMethods {
 		return null;
 	}
 
-
 	// Return the number of words in String s that end with String substring
 	// You can assume there are no punctuation marks between words
 	public static int wordsEndsWithSubstring(String s, String substring) {
 		return 0;
 	}
-	
 
 	// Given String s, return the number of characters between the first occurrence
 	// of String substring and the final occurrence
@@ -91,14 +118,13 @@ public class StringMethods {
 		return 0;
 	}
 
-
 	// Return true if String s is a palindrome
 	// palindromes are words or phrases are read the same forward as backward.
 	// HINT: ignore/remove all punctuation and spaces in the String
 	public static boolean palindrome(String s) {
 		return true;
 	}
-	
+
 }
 
 class Utilities {
